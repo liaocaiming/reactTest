41bc719111977657f6557b50894f0582db855a48
CaimingLiao <CaimingLiao@sf-express.com>
2020-12-14 19:01:18 +0800

    moblie: 修改pay

--- src/mobile/routes/Pay/index.tsx
+++ src/mobile/routes/Pay/index.tsx
@@ -1,37 +1,38 @@
 import { Button } from "antd";
 import React from "react";
-import { copy } from '@utils/index'
-import IProps from '@typings/react.d'
+import { copy } from "@utils/index";
+import IProps from "@typings/react.d";
 import "./index.less";
 
-export default class App extends React.PureComponent<IProps> {
-  private copy = () => {
-    copy('account');
+export default (props: IProps) => {
+  const copyFn = () => {
+    copy("account");
   };
 
-  private goTo = (url: string) => {
-    const { history } = this.props;
+  const goTo = (url: string) => {
+    const { history } = props;
     return () => {
-      history.push(url)
-    }
-  }
+      history.push(url);
+    };
+  };
 
-  public render() {
-    return (
+  return (
+    <div>
       <div>
-        <div>
-          <img src="" alt="usdt收款码" />
-        </div>
-        <div>
-          <input type="text" style={{ display: "none" }} id="input" />
-          <span>
-            收款地址: <span id="account">4343434</span>
-          </span>
-          <Button onClick={this.copy}>复制</Button>
-        </div>
-
-        <div><Button>下一步</Button> <Button onClick={this.goTo('/mobile/home')}>跳过</Button></div>
+        <img src="" alt="usdt收款码" />
       </div>
-    );
-  }
-}
+      <div>
+        <input type="text" style={{ display: "none" }} id="input" />
+        <span>
+          收款地址: <span id="account">4343434</span>
+        </span>
+        <Button onClick={copyFn}>复制</Button>
+      </div>
+
+      <div>
+        <Button>下一步</Button>{" "}
+        <Button onClick={goTo("/mobile/home")}>跳过</Button>
+      </div>
+    </div>
+  );
+};
