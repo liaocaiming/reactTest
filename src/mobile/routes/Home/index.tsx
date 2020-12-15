3364b38ae94e32dd9aae2711ae3702c078589c89
liaocaiming <1963182458@qq.com>
2020-12-15 23:42:34 +0800

    mobile: home头部

--- src/mobile/routes/Home/index.tsx
+++ src/mobile/routes/Home/index.tsx
@@ -1,21 +1,28 @@
-import React from "react";
-import { InputItem, Button } from "antd-mobile";
+import React, { useState } from "react";
 import IProps from "@typings/react.d";
-// import { createForm } from "rc-form";
-import { connect } from "@containers/appScreen";
+import { Tabs } from './components/index';
+import { tabList } from './constants';
+import './index.less';
+import logoIcon from './images/home-logo-icon.png'
+import UIcon from './images/home-u-icon.png';
+import SetICon from './images/home-set-icon.png';
 
-@connect()
-export default class App extends React.PureComponent<IProps> {
-  private goNext = () => {
-    const { history } = this.props;
-    history.push("/mobile/pay");
-  };
-
-  public render() {
-    return (
-      <div>
-        home
-      </div>
-    );
+export default (props: IProps) => {
+  const [type, setType] = useState('hold');
+  const onTabChange = (item) => {
+    setType(item.name)
   }
+
+  return (
+    <div className='mb-home'>
+      <div className="header">
+        <div className='clearfix icon-container'>
+          <img className='fl logo-icon' src={logoIcon} />
+          <img className='fr set-icon' src={SetICon} />
+          <img className='fr u-icon' src={UIcon} />
+        </div>
+        <Tabs list={tabList} activeKey={type} onChange={onTabChange} />
+      </div>
+    </div>
+  );
 }
