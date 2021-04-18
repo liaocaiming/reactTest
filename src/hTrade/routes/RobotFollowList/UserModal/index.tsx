efe5b8a2c5a38fbb0649c3d3125b4727c13fd6fa
liaocaiming <1963182458@qq.com>
2021-04-18 20:21:47 +0800

    feat(htrade): 机器人新增

--- src/hTrade/routes/RobotFollowList/UserModal/index.tsx
+++ src/hTrade/routes/RobotFollowList/UserModal/index.tsx
@@ -1,4 +1,5 @@
 import React, { memo, useState } from 'react';
+
 import { Modal } from 'antd'
 
 import { ModalProps } from 'antd/lib/modal'
@@ -13,7 +14,15 @@ import api from '@src/hTrade/config/api';
 
 import { FormInstance } from 'antd/lib/form';
 
-import { getFormItems } from '../utils';
+import { constants } from "@utils/index";
+
+
+interface IOptions {
+  width?: number;
+  disabled?: boolean;
+  operateType?: "edit" | "add";
+}
+
 
 interface IProps extends ModalProps {
   detail?: any;
@@ -22,16 +31,148 @@ interface IProps extends ModalProps {
   onSuccess: () => void;
 }
 
+
+
 export default memo((props: IProps) => {
   const { detail, visible, actions, onCancel, onSuccess, operateType } = props;
   const [form, setForm] = useState<FormInstance>();
+
+  const [userInfo, setUserInfo] = useState<any>({});
+
+
+  const getFormItems = (options?: IOptions) => {
+    const { width = 300, operateType } = options || {};
+    const disabled = operateType === "edit";
+
+    const formData: AppFormItemOptions[] = [
+      {
+        name: "email",
+        label: "邮箱",
+        editable: !disabled,
+        rules: [
+          {
+            required: true,
+            message: "请输入",
+            whitespace: true,
+          },
+        ],
+        eleAttr: {
+          placeholder: "请输入",
+          onBlur: () => {
+            form?.validateFields(['email']).then((values: any) => {
+              actions.get(api.userList, values).then((json) => {
+                const [item = {}] = json.data || [];
+                console.log(item, 'item');
+
+                setUserInfo(item)
+              })
+            })
+          },
+          disabled,
+          style: {
+            width,
+          },
+        },
+      },
+
+      {
+        name: "biance_user_id",
+        label: "币安uid",
+        editable: false,
+        render: () => {
+          return <span>{userInfo.binance_user_id}</span>
+        }
+      },
+
+      {
+        name: "binance_key",
+        label: "API Key",
+        // isShow: !disabled,
+        rules: [
+          {
+            required: !disabled,
+            message: "请输入",
+            whitespace: true,
+          },
+        ],
+        eleAttr: {
+          placeholder: "请输入API Key",
+          style: {
+            width,
+          },
+        },
+      },
+      {
+        name: "binance_secret",
+        label: "Secret Key",
+        // isShow: !disabled,
+        rules: [
+          {
+            required: !disabled,
+            message: "请输入",
+            whitespace: true,
+          },
+        ],
+        eleAttr: {
+          placeholder: "请输入Secret Key",
+          style: {
+            width,
+          },
+        },
+      },
+
+      {
+        name: "amount",
+        label: "每单金额",
+        rules: [
+          {
+            required: true,
+            message: "请输入",
+            whitespace: true,
+          },
+          {
+            pattern: constants.pattern.positiveNum,
+            message: "请输入正整数",
+          },
+        ],
+        eleAttr: {
+          placeholder: "请输入API Key",
+          style: {
+            width,
+          },
+        },
+      },
+
+      // {
+      //   name: "remark",
+      //   label: "备注",
+      //   type: "textArea",
+      //   eleAttr: {
+      //     placeholder: "请输入",
+      //     style: {
+      //       width,
+      //     },
+      //   },
+      // },
+    ];
+
+    return formData;
+  };
+
 
   const formData: AppFormItemOptions[] = getFormItems({
     operateType
   });
 
+
   const onFinish = (params: any) => {
-    actions.post(api.addAndUpdateUser, params).then(() => {
+    const { operateType } = props;
+    const map = {
+      add: api.bots,
+      edit: api.bots_update
+    }
+
+    actions.post(map[operateType || 'add'], { user_id: detail.id, ...params }).then(() => {
       onSuccess();
     })
   }
@@ -47,7 +188,7 @@ export default memo((props: IProps) => {
         labelCol={{ span: 6 }}
         submitButton={null}
         onFinish={onFinish}
-        updateStore={detail}
+        updateStore={{ ...userInfo, ...detail }}
         onReady={(form) => {
           setForm(form)
         }} />
