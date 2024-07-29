648e72d8100c7d0fb60ba58c6a231120f23a1783
liaocaiming <1963182458@qq.com>
2024-07-29 23:25:02 +0800

    feat: build

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -73,7 +73,7 @@ module.exports = options => {
     },
     output: {
       filename: 'js/[name].[hash].js',
-      path: utils.resolve(`docs/`),
+      path: utils.resolve(`./`),
     },
 
     optimization: {
